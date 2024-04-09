996d23b440ecdc5720389749a7788714effdbc3b
Kehbuma15 <kehbuma185@gmail.com>
2024-04-09 23:45:43 +0100

    basics of form handling

--- src/Component/Form.js
+++ src/Component/Form.js
@@ -1,11 +1,62 @@
 import React, { Component } from 'react'
 
  class Form extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       username: '',
+       comments: '',
+       topic: 'React'
+
+    }
+  }
+
+  handleUsernameChange = (event) =>{
+      this.setState({
+        username: event.target.value
+      })
+  }
+
+  handleCommentsChange = event => {
+    this.setState({
+      comments : event.target.value
+    })
+  }
+
+  handleTopicChange = event => {
+    this.setState({
+      topic: event.target.value
+    })
+  }
+  handleSubmit = event => {
+    alert(`${this.state.username} ${this.state.comments} ${this.state.topic}`)
+    event.preventDefault()
+  }
+  
   render() {
+    const {username, comments, topic} = this.state
     return (
-      <div>
-        Form component
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <div> 
+          <label>Username</label>
+          <input type='text' value={username} onChange={this.handleUsernameChange}/>
+        </div>
+        <div>
+          <label>Comments</label>
+          <textarea value={comments} onChange={this.handleCommentsChange}></textarea>
+        </div>
+        <div>
+          <label>Topic</label>
+          <select value={topic} onChange={this.handleTopicChange}>
+            <option value= "react">React</option>
+            <option value= "angular">angular</option>
+            <option value= "vue">Vue</option>
+          </select>
+        </div>
+        <button value= 'submit'>Submit</button>
+      </form>
     )
   }
 }
